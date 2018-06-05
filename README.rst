56ea81f7e2d814281db9cbee8a56c38297791cf7
chapdbr <bruno.chapdelaine@usherbrooke.ca>
2018-06-05 11:25:55 -0400

    readme and setup updated

--- README.rst
+++ README.rst
@@ -1,8 +1,9 @@
-NI USB DAQ DATALOGGER
+ARDUINO DATALOGGER
 =====================
-This software provides data logging capabilities and real-time plotting
-of incoming data from a NI USB DAQ hooked up with an ATI FT sensor.
+This software logs incoming data from an Arduino microcontroller
+ connected via a serial port to a computer. It automatically connects
+ to the first Arduino detected.
 
-To install using pip, use the following command: py -m pip install https://github.com/chapdbr/ni-usb-datalogger.git
+To install using pip, use the following command: py -m pip install https://github.com/chapdbr/arduino-datalogger.git
 
 It will install all the required dependencies.
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='ni-usb-datalogger',
+    name='arduino-datalogger',
     version='1.0',
-    packages=['ni-usb-datalogger'],
-    install_requires=['nidaqmx', 'numpy', 'matplotlib'],
-    url='https://github.com/chapdbr/NI-USB-DAQ-DATA-LOGGER',
+    packages=['arduino-datalogger'],
+    install_requires=['pyserial'],
+    url='https://github.com/chapdbr/arduino-datalogger.git',
     license='MIT',
     author='Bruno Chapdelaine',
     author_email='bruno.chapdelaine@gmail.com',
-    description='This programs reads voltage data from a National Instrument USB-6003 ADC and converts it to F/T using the calibration matrix from ATI for the ATI mini 45 Titanium.',
+    description='This software logs incoming data from an Arduino microcontroller connected via a serial port to a computer.',
     include_package_data=True
 )
